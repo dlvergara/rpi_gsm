8a5901180493b01fabb05eec09cd661f9868eaee
David Leonardo Vergara <david.l.vergara@gmail.com>
2018-10-27 19:15:15 -0500

    initial commit

---
+++ main.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: David Leonardo V
+ * Date: 27/10/2018
+ * Time: 7:10 PM
+ */
+
+class main
+{
+    public function run()
+    {
+    }
+}
+
+$obj = new main();
+$obj->run();
