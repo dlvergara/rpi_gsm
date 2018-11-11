1b4636d6b851c2bf001176468e535729857f6b0a
David Leonardo Vergara <david.l.vergara@gmail.com>
2018-11-11 17:42:56 -0500

    mode

--- main.php
+++ main.php
@@ -26,28 +26,25 @@ class main
         //$serial->sendMessage("AT", 1);
         //var_dump($serial->readPort());
 
-        //$serial->sendMessage("AT+CMGF=1\n\r");
-        //file_put_contents("CMGF_".date('Y-m-d-H-i-s').".txt", $serial->readPort());
-
         $serial->sendMessage("AT+CMGL=\"REC UNREAD\",0\n\r");
-        file_put_contents("CMGL_".date('Y-m-d-H-i-s').".txt", $serial->readPort());
+        //file_put_contents("CMGL_".date('Y-m-d-H-i-s').".txt", $serial->readPort());
     }
 
     /**
      * @param PhpSerial $serial
      * @param $messageIndex
      */
-    public function deleteMessage( PhpSerial $serial, $messageIndex )
+    public function deleteMessage(PhpSerial $serial, $messageIndex)
     {
         //CMGD
         $serial->sendMessage("AT+CMGD={$messageIndex}\n\r");
-        file_put_contents("CMGD_".date('Y-m-d-H-i-s').".txt", $serial->readPort());
+        file_put_contents("CMGD_" . date('Y-m-d-H-i-s') . ".txt", $serial->readPort());
     }
 
     /**
      * @param PhpSerial $serial
      */
-    private function write(PhpSerial $serial)
+    private function write(PhpSerial $serial, array $command)
     {
         //AT+CMGS="+85291234567",145<CR>This is an example for illustrating the syntax of the +CMGS AT command in SMS text mode.<Ctrl+z>
         //$serial->sendMessage("AT+CMGS=?\n\r");//,145\n\r
@@ -55,13 +52,17 @@ class main
 
         //$serial->sendMessage("AT+CMGF=1\n\r");
 
-        $serial->sendMessage("AT+CMGS=\"982841966\"\n\r");//,145\n\r
-        sleep(1);
-        $serial->sendMessage("mensaje");
-        $serial->sendMessage("\x1A"); # Enable to send SMS
-        //$serial->sendMessage("\x1a"); # Enable to send SMS
-        file_put_contents("CMGS_".date('Y-m-d-H-i-s').".txt", $serial->readPort());
+        foreach ($command as $cmd) {
+            $serial->sendMessage($cmd);
+            sleep(1);
+        }
+        //file_put_contents("CMGS_".date('Y-m-d-H-i-s').".txt", $serial->readPort());
 
+        //$serial->sendMessage("AT+CMGS=\"982841966\"\n\r");//,145\n\r
+        //sleep(1);
+        //$serial->sendMessage("mensaje");
+        //$serial->sendMessage("\x1A"); # Enable to send SMS
+        //$serial->sendMessage("\x1a"); # Enable to send SMS
         //file_put_contents("CMGS2_".date('Y-m-d-H-i-s').".txt", $serial->readPort());
 
         # Sending a message to a particular Number
@@ -80,12 +81,29 @@ class main
         #print rcv
     }
 
+    public function sendSms(PhpSerial $serial)
+    {
+        $command[] = "AT+CMGS=\"982841966\"\n\r";
+        $command[] = "6668701\n\r";
+        $command[] = "\x1A";
+        $this->write($serial, $command);
+    }
+
+    public function callByPhone(PhpSerial $serial)
+    {
+        $command[] = "ATD982841966\n\r";
+        //$command[] = "6668701";
+        //$command[] = "\x1A";
+        $this->write($serial, $command);
+    }
+
     /**
      * Run
      */
-    public function run()
+    public function run($args = array())
     {
         $this->setup();
+        $mode = isset($args[1]) ? $args[1] : 1;
 
         // Let's start the class
         $serial = new PhpSerial();
@@ -98,24 +116,57 @@ class main
         $deviceName = '/dev/serial0';
         //$deviceName = '/dev/ttyAMA0';
         $set = $serial->deviceSet($deviceName);
-        var_dump($serial->_dState); echo chr(10);
+        var_dump($serial->_dState);
+        echo chr(10);
 
         // We can change the baud rate
         $setRate = $serial->confBaudRate(115200);//9600
-        var_dump($serial->_dState); echo chr(10);
+        $serial->confParity("none");
+        $serial->confCharacterLength(8);
+        $serial->confStopBits(1);
+        var_dump($serial->_dState);
+        echo chr(10);
 
         // Then we need to open it
         $open = $serial->deviceOpen('w+');
-        var_dump($serial->_dState); echo chr(10);
+        var_dump($serial->_dState);
+        echo chr(10);
 
         if ($set && $open) {
             // We may need to return if nothing happens for 10 seconds
-            stream_set_timeout($serial->_dHandle, 10);
+            stream_set_timeout($serial->_dHandle, 1);
+
+            $serial->serialflush();
+            //$serial->sendMessage("AT+CMGF=1\n\r");
+            //file_put_contents("CMGF_" . date('Y-m-d-H-i-s') . ".txt", $serial->readPort());
+
+            switch ($mode) {
+                case 1:
+                    $this->read($serial);
+                    break;
+                case 'read':
+                    $this->read($serial);
+                    break;
+                case 2:
+                    $this->sendSms($serial);
+                    break;
+                case 'write':
+                    $this->sendSms($serial);
+                    break;
+                default:
+                    $this->read($serial);
+                    break;
+            }
+
             echo '----', chr(10);
+            //$this->callByPhone($serial);
+            //sleep(60);
+            //echo '----.----', chr(10);
+            //$this->sendSms($serial);
+            //sleep(2);
+            //echo '----.----', chr(10);
             //$this->read($serial);
-            echo '----.----', chr(10);
-            $this->write($serial);
-            echo '----.----', chr(10);
+            //echo '----.----', chr(10);
             /*
             do {
                 echo '----', chr(10);
@@ -129,10 +180,10 @@ class main
         }
 
         // If you want to change the configuration, the device must be closed
-        $serial->deviceClose();
-        echo "finish.".chr(10);
+        $close = $serial->deviceClose();
+        echo "finish -> " . print_r($close, true) . chr(10);
     }
 }
 
 $obj = new main();
-$obj->run();
+$obj->run($argv);
